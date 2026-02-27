8ac7e413c3d7231a1805b96f6498d30336901fb4
mparadina <milan.paradina@microblink.com>
2026-02-27 16:16:00 +0100

    create ios bindings

--- BlinkCard/ios/blinkcard_flutter/Package.swift
+++ BlinkCard/ios/blinkcard_flutter/Package.swift
@@ -6,27 +6,21 @@ import PackageDescription
 let package = Package(
     name: "blinkcard_flutter",
     platforms: [
-        .iOS("13.0")
+        .iOS("16.0")
     ],
     products: [
         .library(name: "blinkcard-flutter", targets: ["blinkcard_flutter"])
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/BlinkCard/blinkcard-ios.git", exact: .init(3000, 0, 0))
+    ],
     targets: [
         .target(
             name: "blinkcard_flutter",
-            dependencies: [],
-            resources: [
-                // If your plugin requires a privacy manifest, for example if it uses any required
-                // reason APIs, update the PrivacyInfo.xcprivacy file to describe your plugin's
-                // privacy impact, and then uncomment these lines. For more information, see
-                // https://developer.apple.com/documentation/bundleresources/privacy_manifest_files
-                // .process("PrivacyInfo.xcprivacy"),
-
-                // If you have other resources that need to be bundled with your plugin, refer to
-                // the following instructions to add them:
-                // https://developer.apple.com/documentation/xcode/bundling-resources-with-a-swift-package
-            ]
+            dependencies: [
+                .product(name: "BlinkCardUX", package: "blinkcard-ios")
+            ],
+            resources: []
         )
     ]
 )
